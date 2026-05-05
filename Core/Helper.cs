427b3540fca6e37d1a6c1a437fe8c5bcd2f235c0
dangdoday <nguyenhaidangbkx@gmail.com>
2026-05-05 09:15:11 +0700

    Add camera control and light management features
    - Implemented SettingsPanel for camera settings and controls.
    - Created IndustrialLightApp for managing light operations and camera integration.
    - Added configuration file (config.ini) for storing user settings.
    - Introduced network communication for light control via TCP.
    - Enabled continuous and single shot camera operations.
    - Added logging functionality for system events and errors.
    - Integrated UI components for user interaction with camera and light settings.
    - Included necessary DLLs and resource files for application functionality.

---
+++ Core/Helper.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Windows.Forms;
+
+namespace LightControllerApp.Core
+{
+    class Helper
+    {
+        public static void ShowException(Exception exception)
+        {
+            MessageBox.Show("Exception caught:\n" + exception.Message, "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+        }
+
+        public static void ShowMessage(String message)
+        {
+            MessageBox.Show(message, "Information", MessageBoxButtons.OK, MessageBoxIcon.Information);
+        }
+    }
+}
